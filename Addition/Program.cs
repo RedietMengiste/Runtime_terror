155e3f21439d8e1c0f5500110efccb91f82043e3
Rediet <49783263+RedietMengiste@users.noreply.github.com>
2021-05-22 12:11:02 +0300

    Addition and subtraction

---
+++ Addition/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Addition
+{
+    class Program
+    
+    {
+       
+         static void Main(string[] args)
+        {
+            
+            System.Console.WriteLine("Enter the first number: ");
+            string num1 = Console.ReadLine();
+            System.Console.WriteLine("Enter the second number: ");
+            string num2 = Console.ReadLine();
+            
+            System.Console.WriteLine(addnums(num1,num2));
+
+           
+        }
+        static string addnums(string x, string y){
+             //int x = Convert.ToInt32(num1);
+            
+           string z="";
+            int sum=0;
+            int len1 = x.Length;
+            int len2 = y.Length;
+            int carry=0;
+            while(len1 >= 1 || len2 >= 1){
+                int i=0,j = 0;
+                if (len1 >0){
+                     i=x[len1-1]-'0';
+                  // System.Console.WriteLine(i);
+                    len1--;
+                }
+               if(len2 > 0){
+                j = y[len2-1]-'0';
+                //System.Console.WriteLine(j);
+                len2--;
+               }
+               
+                sum = i + j + carry;
+               // System.Console.WriteLine("carry"+carry);
+            
+                if (sum >= 10){   
+                    sum = sum % 10;
+                    carry = 1;
+                }
+                else{
+                    carry = 0;
+                    sum=sum+carry;
+                }
+                
+                z=Convert.ToString(sum)+z;
+
+          
+            }
+             if(carry==1){
+                    
+                      z="1"+z;
+                   
+
+                }
+                
+            return z;
+            
+          
+           
+        }
+ 
+    
+    }
+}
---
+++ Subtraction/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Subtraction
+{
+    class Program
+    {
+        static void Main(String[] args) {
+		 
+            System.Console.WriteLine("Enter the first number: ");
+            string num1 = Console.ReadLine();
+            System.Console.WriteLine("Enter the second number: ");
+            string num2 = Console.ReadLine();
+            
+            System.Console.WriteLine(subnums(num1,num2));
+		
+	}
+ 
+	  static string subnums(string x, string y){
+             //int x = Convert.ToInt32(num1);
+            
+           string z="";
+            int diff=0;
+            int len1 = x.Length;
+            int len2 = y.Length;
+            int borrow=0;
+            while(len1 >= 1 || len2 >= 1){
+				
+                int i=0,j = 0;
+                if (len1 >0){
+                    
+					 if(borrow==1){
+						i=x[len1-1]-'0'-1;
+						// borrow = 0;
+						len1--;
+					 }
+					else{
+						i=x[len1-1]-'0';
+						borrow=0;
+						len1--;
+					}
+					System.Console.WriteLine(i);
+						
+                }
+				
+               if(len2 > 0){
+                j = y[len2-1]-'0';
+                System.Console.WriteLine(j);
+                len2--;
+               }
+			  
+				
+               if (i<j){
+                diff = (i+10) - j ;
+				borrow = 1;
+				
+				// j = (y[len2-1]-'0')-1;
+			   }
+				else
+				    diff=i-j;
+					
+                
+				System.Console.WriteLine("borrow "+borrow);
+            
+                // z=Convert.ToString(diff);
+                z+=Convert.ToString(diff);
+
+          
+            }
+			
+            //  if(borrow==1){
+                    
+            //           z="-"+z;
+                   
+
+            //     }
+                
+            return z;
+            
+        //   return null;
+           
+        }
+ 
+
+}
+}
